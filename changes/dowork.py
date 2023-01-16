5429df18d5721e6fc904f881fe070bd7f5fc0f9f
leejiwon1125 <leejiwon1125@postech.ac.kr>
2023-01-16 11:20:36 +0900

    checkout.yml and dowork.py changed : argument index changed between prtitle and rpnumber

--- changes/dowork.py
+++ changes/dowork.py
@@ -1,11 +1,11 @@
 import sys
 
-pr_title = sys.argv[1] #제목
-pr_number = sys.argv[2] #번호
+pr_number = sys.argv[1] #번호
+pr_title = sys.argv[2] #제목
 
 index = pr_title.find(':') 
 
 new_title = pr_number + '.' + pr_title[0:index] +'.md'
 file = open(new_title,"w")
-file.write(pr_title[index+1:])
+file.write("파일에 뭔가를 쓸 수 있어야한다...")
 file.close()
