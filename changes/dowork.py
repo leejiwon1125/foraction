2e556b07ee83fa54f5ff599d99acce02e45b51b7
leejiwon1125 <leejiwon1125@postech.ac.kr>
2023-01-16 12:38:37 +0900

    modified checkout.yml and dowork.py -> printing message changed

--- changes/dowork.py
+++ changes/dowork.py
@@ -7,5 +7,5 @@ index = pr_title.find(':')
 
 new_title = pr_number + '.' + pr_title[0:index] +'.md'
 file = open(new_title,"w")
-file.write("파일에 뭔가를 쓸 수 있어야한다...")
+file.write("this PR is not yet merged")
 file.close()
